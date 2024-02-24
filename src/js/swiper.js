cf40a6cd2f0e31ed31e6fa33254d432ef9fbeae7
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-02-24 14:22:00 +0200

    markup completed

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -6,47 +6,12 @@ import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
 const swiper = new Swiper(".mySwiper", {
-  loop: true,
   spaceBetween: 100,
 
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-
-  autoplay: {
-    delay: 5000,
-    pauseOnMouseEnter: true,
-  },
-
-  // breakpoints: {
-  //   320: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 100,
-  //   },
-  //   1279: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 100,
-  //   },
-  // },
-});
-
-const swiper2 = new Swiper(".mySwiper2", {
-  // loop: true,
-
-  // autoHeight: true,
-
-  spaceBetween: 100,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  // autoplay: {
-  //   delay: 5000,
-  //   pauseOnMouseEnter: true,
-  // },
 
   navigation: {
     nextEl: ".swiper-button-next",
@@ -64,3 +29,37 @@ const swiper2 = new Swiper(".mySwiper2", {
   //   },
   // },
 });
+
+// const swiper2 = new Swiper(".mySwiper2", {
+//   // loop: true,
+
+//   // autoHeight: true,
+
+//   spaceBetween: 100,
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+
+//   // autoplay: {
+//   //   delay: 5000,
+//   //   pauseOnMouseEnter: true,
+//   // },
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   // breakpoints: {
+//   //   320: {
+//   //     slidesPerView: 1,
+//   //     spaceBetween: 100,
+//   //   },
+//   //   1279: {
+//   //     slidesPerView: 3,
+//   //     spaceBetween: 100,
+//   //   },
+//   // },
+// });
--- src/js/timer.js
+++ src/js/timer.js
@@ -65,34 +65,34 @@ function initializeClockTwo(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-// function initializeClockThree(id, endtime) {
-//   var clock = document.getElementById(id);
-//   // var daysSpan = clock.querySelector(".days-three");
-//   var hoursSpan = clock.querySelector(".hours-three");
-//   var minutesSpan = clock.querySelector(".minutes-three");
-//   var secondsSpan = clock.querySelector(".seconds-three");
+function initializeClockThree(id, endtime) {
+  var clock = document.getElementById(id);
+  // var daysSpan = clock.querySelector(".days-three");
+  var hoursSpan = clock.querySelector(".hours-three");
+  var minutesSpan = clock.querySelector(".minutes-three");
+  var secondsSpan = clock.querySelector(".seconds-three");
 
-//   function updateClock() {
-//     var t = getTimeRemaining(endtime);
-//     daysSpan.innerHTML = ("0" + t.days).slice(-2);
-//     hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-//     minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
-//     secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+    // daysSpan.innerHTML = ("0" + t.days).slice(-2);
+    hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+    minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
 
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//       var deadline = new Date(Date.parse(endtime) + 24 * 60 * 60 * 1000);
-//       initializeClockThree("countdown-three", deadline);
-//     }
-//   }
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+      var deadline = new Date(Date.parse(endtime) + 24 * 60 * 60 * 1000);
+      initializeClockThree("countdown-three", deadline);
+    }
+  }
 
-//   updateClock();
-//   var timeinterval = setInterval(updateClock, 1000);
-// }
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
 
 var deadline = "February 02 2025 00:00:00 GMT+0200";
 initializeClock("countdown", deadline);
 initializeClockTwo("countdown-two", deadline);
-// initializeClockThree("countdown-three", deadline);
+initializeClockThree("countdown-three", deadline);
 
 // timer - two;
