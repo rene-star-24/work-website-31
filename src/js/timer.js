7deca26f7057e99d9a76d00e88254660410530f5
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-03-12 13:03:09 +0200

    change timer time

--- src/js/timer.js
+++ src/js/timer.js
@@ -2,13 +2,13 @@
 
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
-  // var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
   return {
     total: t,
-    // days: days,
+    days: days,
     hours: hours,
     minutes: minutes,
     seconds: seconds,
@@ -17,14 +17,14 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  // var daysSpan = clock.querySelector(".days");
+  var daysSpan = clock.querySelector(".days");
   var hoursSpan = clock.querySelector(".hours");
   var minutesSpan = clock.querySelector(".minutes");
   var secondsSpan = clock.querySelector(".seconds");
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-    // daysSpan.innerHTML = ("0" + t.days).slice(-2);
+    daysSpan.innerHTML = ("0" + t.days).slice(-2);
     hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
     minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
     secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
@@ -42,14 +42,14 @@ function initializeClock(id, endtime) {
 
 function initializeClockTwo(id, endtime) {
   var clock = document.getElementById(id);
-  // var daysSpan = clock.querySelector(".days-two");
+  var daysSpan = clock.querySelector(".days-two");
   var hoursSpan = clock.querySelector(".hours-two");
   var minutesSpan = clock.querySelector(".minutes-two");
   var secondsSpan = clock.querySelector(".seconds-two");
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-    // daysSpan.innerHTML = ("0" + t.days).slice(-2);
+    daysSpan.innerHTML = ("0" + t.days).slice(-2);
     hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
     minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
     secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
@@ -67,14 +67,14 @@ function initializeClockTwo(id, endtime) {
 
 function initializeClockThree(id, endtime) {
   var clock = document.getElementById(id);
-  // var daysSpan = clock.querySelector(".days-three");
+  var daysSpan = clock.querySelector(".days-three");
   var hoursSpan = clock.querySelector(".hours-three");
   var minutesSpan = clock.querySelector(".minutes-three");
   var secondsSpan = clock.querySelector(".seconds-three");
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-    // daysSpan.innerHTML = ("0" + t.days).slice(-2);
+    daysSpan.innerHTML = ("0" + t.days).slice(-2);
     hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
     minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
     secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
@@ -90,7 +90,7 @@ function initializeClockThree(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = "February 02 2025 00:00:00 GMT+0200";
+var deadline = "March 14 2024 12:00:00 GMT+0200";
 initializeClock("countdown", deadline);
 initializeClockTwo("countdown-two", deadline);
 initializeClockThree("countdown-three", deadline);
