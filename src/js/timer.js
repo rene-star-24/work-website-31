47e3dc75fc6caa0a3c7013e8dd13e3b11f963e1a
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-04-03 19:44:13 +0300

    update timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,42 +1,44 @@
-// timer-one
-
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
   return {
     total: t,
-    days: days,
     hours: hours,
     minutes: minutes,
     seconds: seconds,
   };
 }
 
-function initializeClock(id, endtime) {
+function initializeClock(
+  id,
+  endtime,
+  hoursSelector,
+  minutesSelector,
+  secondsSelector
+) {
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector(".days");
-  var hoursSpan = clock.querySelector(".hours");
-  var minutesSpan = clock.querySelector(".minutes");
-  var secondsSpan = clock.querySelector(".seconds");
+  var hoursSpan = clock.querySelector(hoursSelector);
+  var minutesSpan = clock.querySelector(minutesSelector);
+  var secondsSpan = clock.querySelector(secondsSelector);
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-    // daysSpan.innerHTML = ("0" + t.days).slice(-2);
-    // hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-    // minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
-    // secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
-    daysSpan.innerHTML = "00";
-    hoursSpan.innerHTML = "00";
-    minutesSpan.innerHTML = "00";
-    secondsSpan.innerHTML = "00";
+    hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+    minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
       var deadline = new Date(Date.parse(endtime) + 24 * 60 * 60 * 1000);
-      initializeClock("countdown", deadline);
+      initializeClock(
+        id,
+        deadline,
+        hoursSelector,
+        minutesSelector,
+        secondsSelector
+      );
     }
   }
 
@@ -44,67 +46,19 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-function initializeClockTwo(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector(".days-two");
-  var hoursSpan = clock.querySelector(".hours-two");
-  var minutesSpan = clock.querySelector(".minutes-two");
-  var secondsSpan = clock.querySelector(".seconds-two");
-
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-    // daysSpan.innerHTML = ("0" + t.days).slice(-2);
-    // hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-    // minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
-    // secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
-    daysSpan.innerHTML = "00";
-    hoursSpan.innerHTML = "00";
-    minutesSpan.innerHTML = "00";
-    secondsSpan.innerHTML = "00";
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-      var deadline = new Date(Date.parse(endtime) + 24 * 60 * 60 * 1000);
-      initializeClockTwo("countdown-two", deadline);
-    }
-  }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
-
-function initializeClockThree(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector(".days-three");
-  var hoursSpan = clock.querySelector(".hours-three");
-  var minutesSpan = clock.querySelector(".minutes-three");
-  var secondsSpan = clock.querySelector(".seconds-three");
-
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-    // daysSpan.innerHTML = ("0" + t.days).slice(-2);
-    // hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-    // minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
-    // secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
-    daysSpan.innerHTML = "00";
-    hoursSpan.innerHTML = "00";
-    minutesSpan.innerHTML = "00";
-    secondsSpan.innerHTML = "00";
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-      var deadline = new Date(Date.parse(endtime) + 24 * 60 * 60 * 1000);
-      initializeClockThree("countdown-three", deadline);
-    }
-  }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
-
-var deadline = "March 14 2025 12:00:00 GMT+0200";
-initializeClock("countdown", deadline);
-initializeClockTwo("countdown-two", deadline);
-initializeClockThree("countdown-three", deadline);
-
-// timer - two;
+var deadline = "September 31 2024 00:00:00 GMT+0200";
+initializeClock("countdown", deadline, ".hours", ".minutes", ".seconds");
+initializeClock(
+  "countdown-two",
+  deadline,
+  ".hours-two",
+  ".minutes-two",
+  ".seconds-two"
+);
+initializeClock(
+  "countdown-three",
+  deadline,
+  ".hours-three",
+  ".minutes-three",
+  ".seconds-three"
+);
