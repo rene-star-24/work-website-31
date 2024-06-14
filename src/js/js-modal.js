1a92a8972ddf78acddee36ea69d399459635a417
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-06-14 14:41:31 +0300

    added modal btns

--- src/js/js-modal.js
+++ src/js/js-modal.js
@@ -1,29 +1,32 @@
 (() => {
   const refs = {
-    openModalBtn1: document.querySelectorAll("[data-modal-open-one]"),
-    // openModalBtn2: document.querySelectorAll("[data-modal-open-two]"),
-    closeModalBtn1: document.querySelector("[data-modal-close1]"),
-    // closeModalBtn2: document.querySelector("[data-modal-close2]"),
-    modal1: document.querySelector("[data-modal1]"),
-    // modal2: document.querySelector("[data-modal2]"),
+    openModalBtn1: document.querySelectorAll("[data-modal-open]"),
+    closeModalBtn1: document.querySelector("[data-modal-close]"),
+    modal1: document.querySelector("[data-modal]"),
+    // backdrop: document.querySelector(".backdrop"),
   };
 
   refs.openModalBtn1.forEach((item) =>
     item.addEventListener("click", toggleModal1)
   );
 
-  // refs.openModalBtn2.forEach((item) =>
-  //   item.addEventListener("click", toggleModal2)
-  // );
-
   refs.closeModalBtn1.addEventListener("click", toggleModal1);
-  // refs.closeModalBtn2.addEventListener("click", toggleModal2);
+  // refs.backdrop.addEventListener("click", onBackdropClick);
 
   function toggleModal1() {
     refs.modal1.classList.toggle("is-hidden");
+    document.body.classList.toggle("modal-open");
   }
 
-  // function toggleModal2() {
-  //   refs.modal2.classList.toggle("is-hidden");
+  // function onBackdropClick(event) {
+  //   if (event.currentTarget === event.target) {
+  //     toggleModal1();
+  //   }
   // }
+
+  if ("ontouchstart" in window) {
+    refs.openModalBtn1.forEach((item) =>
+      item.addEventListener("touchstart", toggleModal1)
+    );
+  }
 })();
