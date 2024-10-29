920eeebdd96404ee4b4c28a7dc9c9293b061c93d
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-10-29 16:02:36 +0200

    update colors, change font, rework modal

--- src/js/js-modal.js
+++ src/js/js-modal.js
@@ -3,7 +3,7 @@
     openModalBtn1: document.querySelectorAll("[data-modal-open]"),
     closeModalBtn1: document.querySelector("[data-modal-close]"),
     modal1: document.querySelector("[data-modal]"),
-    // backdrop: document.querySelector(".backdrop"),
+    backdrop: document.querySelector(".backdrop"),
   };
 
   refs.openModalBtn1.forEach((item) =>
@@ -11,18 +11,18 @@
   );
 
   refs.closeModalBtn1.addEventListener("click", toggleModal1);
-  // refs.backdrop.addEventListener("click", onBackdropClick);
+  refs.backdrop.addEventListener("click", onBackdropClick);
 
   function toggleModal1() {
     refs.modal1.classList.toggle("is-hidden");
     document.body.classList.toggle("modal-open");
   }
 
-  // function onBackdropClick(event) {
-  //   if (event.currentTarget === event.target) {
-  //     toggleModal1();
-  //   }
-  // }
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      toggleModal1();
+    }
+  }
 
   if ("ontouchstart" in window) {
     refs.openModalBtn1.forEach((item) =>
